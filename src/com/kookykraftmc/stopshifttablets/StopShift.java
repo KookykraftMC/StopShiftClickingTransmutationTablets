c7cedcf419fa4a47f546080a0645582659fb3f2a
SolarShrieking <michael.anible@gmail.com>
2016-06-27 22:15:57 -0400

    Removed Horse references. Will try another way, another day.

--- src/com/kookykraftmc/stopshifttablets/StopShift.java
+++ src/com/kookykraftmc/stopshifttablets/StopShift.java
@@ -5,8 +5,8 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
@@ -14,48 +14,47 @@ import java.util.logging.Logger;
 import static org.bukkit.event.inventory.InventoryAction.HOTBAR_MOVE_AND_READD;
 import static org.bukkit.event.inventory.InventoryAction.HOTBAR_SWAP;
 
-public class StopShift extends JavaPlugin implements Listener
-{
+public class StopShift extends JavaPlugin implements Listener {
     public static final Logger log = Bukkit.getLogger();
-    public void onEnable()
-    {
+
+    public void onEnable() {
         Bukkit.getServer().getPluginManager().registerEvents(this, this);
     }
 
     /**
+     * Debug method to get entity names of mobs hit.
      *
-     * @param e event
-     *          This should only do anything if the player is SolarShrieking.
-     *          Debug AF
+     * @param e Entity(ies)
      */
     @EventHandler
-    public void onInventoryOpen(InventoryOpenEvent e)
-    {
-        if (e.getPlayer().getName().equals("SolarShrieking")) {
-            Player p = (Player) e.getPlayer();
-            p.sendMessage(e.getInventory().getTitle());
-            p.sendMessage(e.getInventory().getName());
-        }
+    public void onEntityInteract(EntityDamageByEntityEvent e) {
+        if (e.getDamager() instanceof Player && ((Player) e.getDamager()).hasPermission("stopshift.debug")) {
+            Player p = (Player) e.getDamager();
+            p.sendMessage(ChatColor.GOLD + e.getEntity().toString());
+            p.sendMessage(ChatColor.RED + e.getEntityType().toString());
 
+        }
     }
-    
+
+    public StopShift() {
+        super();
+    }
+
+    /**
+     *
+     * @param e ClickEvent base
+     */
     @EventHandler
-    public void onShiftClick(InventoryClickEvent e)
-    {
-        /*
-         * This is like the messiest thing I've ever written
-         * I will make it better at some point!
-         */
-        if(e.getInventory() == null || e.getInventory().getTitle() == null) {
+    public void onShiftClick(InventoryClickEvent e) {
+
+        if (e.getInventory() == null || e.getInventory().getTitle() == null) {
             return;
         }
+
         Player p = (Player) e.getWhoClicked();
 
-        if(e.getInventory().getName().equals("Pedestal")
-                || e.getInventory().getName().equalsIgnoreCase("Chest")
-                || e.getInventory().getTitle().equalsIgnoreCase("Chest")) {
-            if(e.getClick().isShiftClick())
-            {
+        if (e.getInventory().getName().equals("Pedestal")) {
+            if (e.getClick().isShiftClick()) {
                 e.setCancelled(true);
                 p.sendMessage(ChatColor.RED + "Bad cat!");
                 p.getWorld().createExplosion(p.getLocation(), 0F, false);
@@ -63,13 +62,11 @@ public class StopShift extends JavaPlugin implements Listener
             }
         }
 
-        if(!e.getInventory().getTitle().equals("container.ee3:transmutationTablet")
-                && !(e.getInventory().getName().equalsIgnoreCase("Chest"))
-                && !(e.getInventory().getTitle().equalsIgnoreCase("Chest")))
+        if (!e.getInventory().getTitle().equals("container.ee3:transmutationTablet"))
             return;
-        if(e.getClick().isShiftClick())
+        if (e.getClick().isShiftClick())
             log.warning(p.getName() + "Tried to shift click with transmutation tablets!");
-        else if(e.getAction() == HOTBAR_SWAP || e.getAction() == HOTBAR_MOVE_AND_READD)
+        else if (e.getAction() == HOTBAR_SWAP || e.getAction() == HOTBAR_MOVE_AND_READD)
             log.warning(p.getName() + "Tried to use the number key transmutation tablet dupe!");
         else
             return;
